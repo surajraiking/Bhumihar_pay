f8d459102dd55a8fd69e8b0f559710308aca10d2
Suraj rai <Surajraiking22@gmail.com>
2026-09-25 23:08:54 +0530

    fix: use dedicated Capacitor web directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+
 const config: CapacitorConfig = {
   appId: 'com.surajraipay.app',
   appName: 'Suraj Rai Pay',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false
 };
+
 export default config;
