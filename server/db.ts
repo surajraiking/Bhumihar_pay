bcbcbc23990dd6238ce0ce41c7a6b31b946501f7
Suraj rai <Surajraiking22@gmail.com>
2026-09-25 23:20:49 +0530

    feat: add PostgreSQL transaction helper

---
+++ server/db.ts
@@ -0,0 +1,19 @@
+import { Pool, type PoolClient } from "pg";
+import { env } from "./config.js";
+
+export const pool = new Pool({ connectionString: env.DATABASE_URL, max: 10 });
+
+export async function withTransaction<T>(fn: (client: PoolClient) => Promise<T>): Promise<T> {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const result = await fn(client);
+    await client.query("COMMIT");
+    return result;
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
